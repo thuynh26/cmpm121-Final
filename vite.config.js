9f39b0b5abdc14223700aa44293d85590b61749e
JoshuWoshua1 <jkimpear@ucsc.edu>
2025-11-23 07:00:54 +0000

    vite fix again T-T

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 // https://vitejs.dev/config/
 export default {
-  base: Deno.env.get("REPO_NAME") || "/project",
+  base: Deno.env.get("REPO_NAME") || "/",
   server: {
     port: 3000,
     open: true,
   },
   build: {
-    target: "baseline-widely-available",
+    target: "es2015",
     outDir: "dist",
     sourcemap: true,
   },
